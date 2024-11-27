69748497d22259c14cf921fbfd8ff78a390b0a07
Gustavo Creado Figueroa <gcreado@cellit.cl>
2024-11-27 12:33:25 -0300

    Mostrar URI de conexion DB

--- database/config.js
+++ database/config.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
+    console.log("MongoDB URI:", process.env.MONGODBCNN);
     await mongoose.connect(process.env.MONGODBCNN);
 
     console.log("Conexion a la base de datos");
